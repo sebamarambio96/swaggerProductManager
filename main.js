8a932d619379c1c2a71f830e6650f427418e2cb4
seba.marambio96 <s.marambiosandoval@gmail.com>
2023-07-09 20:16:45 -0400

    1

---
+++ main.js
@@ -0,0 +1,41 @@
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      description:
+        'A simple CRUD API application made with Express and documented with Swagger',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./docs/**/*.yaml'],
+}
+
+const specs = swaggerJsdoc(options)
+
+// const { inspect } = require('node:util')
+// console.log(inspect(specs, false, 10))
+
+//-------------------------------------------------------------------
+
+const express = require('express')
+
+const app = express()
+
+app.get('/productos', (req, res, next) => {
+  res.json([])
+})
+
+//-------------------------------------------------------------------
+
+const swaggerUi = require('swagger-ui-express')
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs))
+
+//-------------------------------------------------------------------
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Documentación disponible en http://localhost:${PORT}/api/docs`)
+})
